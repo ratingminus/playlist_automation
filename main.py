6f73424b8f17ac05f606a2eb54749ff6dd2596ef
ratingminus <mahadi@Wolf.localdomain>
2021-06-28 12:35:57 +0600

    deleted:    add.png 	new file:   add_testing.png 	new file:   done.txt 	modified:   in.txt 	new file:   kids.png 	deleted:    list.png 	modified:   main.py 	new file:   out.txt 	new file:   save.png

--- main.py
+++ main.py
@@ -1,26 +1,41 @@
 from PIL.ImageOps import grayscale
 from sys import stdin
+from numpy import add
 import pyautogui
 
 cnt = 0;
 f = open("in.txt")
+g = open("out.txt", "a")
+h = open("done.txt", "a")
+k = open("done.txt", "r")
+
 for url in f:
+    if url in k:
+        continue
     print("Adding " + url)
-    now = pyautogui.locateOnScreen('new_tab.png', grayscale = True, confidence = .8)
+    now = pyautogui.locateOnScreen('new_tab.png', grayscale = False, confidence = .8)
     pyautogui.click(x = now.left , y = now.top)
     pyautogui.write(url)
     pyautogui.hotkey('enter')
-    now = None
-    while now is None:
-        now = pyautogui.locateOnScreen('add.png', grayscale = True, confidence = .8)
-        # print(now)
-    pyautogui.click(x = now.left, y = now.top)
-    now = None
-    while now is None:
-        now = pyautogui.locateOnScreen('list.png', grayscale = True, confidence = .8)
-        # print(now)
-    pyautogui.click(x = now.left + 20, y = now.top + 20, duration = 0.25)
+    if_kids = None
+    for i in range(30):
+        if_kids = pyautogui.locateOnScreen('kids.png', grayscale = False, confidence = 0.8)
+    if if_kids != None:
+        g.write("\n" + url)
+        continue
+    save_now = None
+    while save_now is None:
+        save_now = pyautogui.locateOnScreen('save.png', grayscale = False, confidence = .8)
+        print("wtf")
+    print(save_now)
+    pyautogui.click(x = save_now.left, y = save_now.top)
+    add_now = None
+    while add_now is None:
+        add_now = pyautogui.locateOnScreen('add_testing.png', grayscale = False, confidence = .8)
+    print(add_now)
+    pyautogui.click(x = add_now.left, y = add_now.top, duration = 0.25)
     cnt = cnt + 1
     print("Added " + str(cnt) + " by now")
+    h.write("\n" + url)
 
 
